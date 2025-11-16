139375bdb188d2b7f3038c359fab7ca8797fe596
SahinShazi <rksahinrone@gmail.com>
2025-11-16 19:25:30 +0600

    update

---
+++ Projects /Pop-up/script.js
@@ -0,0 +1,8 @@
+let popup = document.querySelector('.form');
+
+function openPopup() {
+    popup.classList.add('open-popup' );
+}
+function closePopup() {
+    popup.classList.remove('open-popup' );
+}
